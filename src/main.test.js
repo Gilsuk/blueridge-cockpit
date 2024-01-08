aefe4380d5f7e9b04f837fdaab2b64a95eb733d5
gilsuk <devgilsuk@gmail.com>
2024-01-08 17:45:43 +0900

    prepare test environment for DOM testing

--- src/main.test.js
+++ src/main.test.js
@@ -1,5 +1,17 @@
-import { expect, test } from "vitest";
+import { screen } from "@testing-library/dom";
+import "@testing-library/jest-dom/vitest";
+import { beforeEach, expect, test } from "vitest";
 
 test('테스트 환경이 DOM인지 확인', () => {
     expect(window).not.toBeUndefined();
+})
+
+beforeEach(() => {
+    // window = new Window({})
+})
+
+test('DOM test snippet', () => {
+    const btn = window.document.createElement('button')
+    window.document.body.appendChild(btn)
+    expect(screen.getAllByRole('button')).toHaveLength(1)
 })
