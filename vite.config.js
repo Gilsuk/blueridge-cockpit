56d0da79e0616507fcb71cc2fe0a1a2be09fb208
gilsuk <devgilsuk@gmail.com>
2024-01-08 18:02:08 +0900

    remove dependency happy-dom

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig(({ command, mode }) => {
             }
         },
         test: {
-            environment: 'happy-dom'
+            environment: 'jsdom'
         }
     }
 })
