98a867453fcda4054cde0d5b22c2378a0a5ae65c
gilsuk <devgilsuk@gmail.com>
2024-01-11 18:07:15 +0900

    add sketch of workflow

--- src/main.test.js
+++ src/main.test.js
@@ -1,6 +1,8 @@
 import "@testing-library/jest-dom/vitest";
 import { JSDOM } from "jsdom";
-import { expect, test, vi } from "vitest";
+import { afterEach, expect, test, vi } from "vitest";
+
+afterEach(() => { document.body.innerHTML = '' })
 
 test('JSDOM이 자동으로 load 이벤트를 발생시키는지 확인', async () => {
     const dom = new JSDOM({})
@@ -14,4 +16,26 @@ test('JSDOM이 자동으로 load 이벤트를 발생시키는지 확인', async 
     })
 
     expect(mock).toBeCalledTimes(1)
+})
+
+test('state 에 따라 렌더링 페이지 결정', async () => {
+    const screen = new JSDOM({}).window.document.body
+
+    const permEmitter = new PermissionStatusEmitter()
+    const authEmitter = new AuthStatusEmitter()
+    const $permission = fromEvent(permEmitter)
+    const $auth = fromEvent(authEmitter)
+
+    new App(screen, {
+        states: $permission.join($auth),
+        actor: function (prev, cur) { },
+        pages: [
+            { condition: permEmitter.GRANTED, page: new PermissionGrantedPage() },
+            { condition: permEmitter.DENIED, page: new PermissionDeniedPage() },
+            { condition: permEmitter.DEFAULT, page: new PermisisonDefaultPage() },
+            { condition: authEmitter.UNSIGNED, page: new LoginPage() },
+            { condition: state.DEFAULT, page: new Page() },
+        ]
+    }).start();
+
 })
