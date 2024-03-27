e0e241bafa8e9a69e9585fa6a09845f03553ec26
gilsuk <devgilsuk@gmail.com>
2024-03-27 10:53:00 +0900

    remove unnecessary build configs

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,6 @@ import { defineConfig } from "vite";
 // eslint-disable-next-line no-unused-vars
 export default defineConfig(({ command, mode }) => {
     return {
-        build: {
-            rollupOptions: {
-                input: [
-                    'index.html',
-                ],
-                output: {
-                    entryFileNames: '[name].js',
-                }
-            }
-        },
         test: {
             environment: 'jsdom',
             restoreMocks: true,
