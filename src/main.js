3fe7551fe1e8efaf07508c0afbe86cce1d7e0c5e
gilsuk <devgilsuk@gmail.com>
2024-03-29 17:02:12 +0900

    add service-worker to the project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import '../styles/main.css'
+import sw from './service-worker?worker&url'
 import { PageFactory } from './page'
 import Permission from './permission'
 import Router from './router'
 
-const permission = new Permission()
-const router = new Router();
-new PageFactory(permission, router).getPage().render()
+navigator.serviceWorker.register(sw, {
+    type: import.meta.env.PROD ? 'classic' : 'module'
+}).then((registration) => {
+    const permission = new Permission()
+    const router = new Router();
+    new PageFactory(permission, router).getPage().render()
+})
---
+++ src/service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (event) => {
+    console.log("sw install event: ", event)
+})
+
+self.addEventListener("activate", (event) => {
+    console.log("sw activate event: ", event)
+})
+
+self.addEventListener("fetch", (event) => {
+    console.log("sw fetch event: ", event)
+})
