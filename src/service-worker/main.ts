cd9b8311fade2e51588f630e240f419e441f6d6c
gilsuk <devgilsuk@gmail.com>
2024-04-18 15:09:39 +0900

    tidy up service worker

--- src/service-worker/main.ts
+++ src/service-worker/main.ts
@@ -4,37 +4,28 @@ export default {}
 
 self.addEventListener("install", (event: ExtendableEvent) => {
     event.waitUntil(self.skipWaiting());
-    console.log("sw install event: ", event)
 })
 
 self.addEventListener("activate", (event: ExtendableEvent) => {
     event.waitUntil(self.clients.claim());
-    console.log("sw activate event: ", event)
 })
 
-self.addEventListener("fetch", (event: FetchEvent) => {
-    event.respondWith(fetch(event.request))
-})
+// self.addEventListener("fetch", (event: FetchEvent) => {
+//     event.respondWith(fetch(event.request))
+// })
 
 self.addEventListener("push", (event: PushEvent) => {
-    // const data = event.data?.json()
-    // const notification = data.notification
+    const data = event.data?.json()
+    const notification = data.notification
 
-    // const title = notification.title
-    // const options = {
-    //     body: notification.body,
-    //     icon: notification.image
-    // }
+    const title = notification.title
 
-    // const result = self.registration.showNotification(title, options)
-    const result = self.registration.showNotification("Hi event", {
-        body: "This is contents",
+    const result = self.registration.showNotification(title, {
+        body: notification.body,
+        icon: notification.image || "/images/favicon192.png",
+        badge: "/images/favicon192.png",
     })
     event.waitUntil(result)
-})
-
-self.addEventListener("message", (event: ExtendableMessageEvent) => {
-    console.log("sw message evet: ", event)
 })
 
 self.addEventListener("notificationclick", (event: NotificationEvent) => {
